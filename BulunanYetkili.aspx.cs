ec5bd8f1cbf04619e3c55ab71b81ff06d816b77e
Ecem Ertürk <ecemerturk@users.noreply.github.com>
2018-07-13 12:09:44 +0300

    yetkiliUpdateSon

--- BulunanYetkili.aspx.cs
+++ BulunanYetkili.aspx.cs
@@ -21,16 +21,26 @@ public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        if (Page.IsPostBack)
+        { }
+        else
+        { formu_doldur(); }
+       
+    }
+
+    void formu_doldur()
+    {
         string query = "";
         string nl = System.Environment.NewLine;
         query += nl + "SELECT K.KISI_ID, K.KISI_TC, K.KISI_AD, K.KISI_SOYAD, K.KISI_DOGUM, K.KISI_KGRUBU, Y.YETKILI_MAIL, Y.YETKILI_TELEFON, Y.YETKILI_BRANS";
         query += nl + "FROM KISI K, YETKILI Y";
-        query += nl + "WHERE K.KISI_ID='"+Session["secilen"]+"' AND K.KISI_ID = Y.YETKILI_ID";
-       
+        query += nl + "WHERE K.KISI_ID='" + Session["secilen"] + "' AND K.KISI_ID = Y.YETKILI_ID";
+
         SqlQuery sqlquery = new SqlQuery();
         DataTable datatable = sqlquery.Query(query);
         TextID.Text = datatable.Rows[0][0].ToString();
-        TextTC .Text = datatable.Rows[0][1].ToString();
+        TextTC.Text = datatable.Rows[0][1].ToString();
         TextAd.Text = datatable.Rows[0][2].ToString();
         TextSoyad.Text = datatable.Rows[0][3].ToString();
         TextDogum.Text = datatable.Rows[0][4].ToString();
@@ -41,7 +51,6 @@ public partial class Default2 : System.Web.UI.Page
         TextID.ReadOnly = true;
         TextTC.ReadOnly = true;
     }
-
     protected void ButtonDuzenle_Click(object sender, EventArgs e)
     {
 
@@ -58,22 +67,7 @@ public partial class Default2 : System.Web.UI.Page
         SqlQuery sqlquery1 = new SqlQuery();
         DataTable datatable1 = sqlquery1.Query(query1);
 
-        string query2 = "";
-        query2 += nl + "SELECT K.KISI_ID, K.KISI_TC, K.KISI_AD, K.KISI_SOYAD, K.KISI_DOGUM, K.KISI_KGRUBU, Y.YETKILI_MAIL, Y.YETKILI_TELEFON, Y.YETKILI_BRANS";
-        query2 += nl + "FROM KISI K, YETKILI Y";
-        query2 += nl + "WHERE K.KISI_ID='" + Session["secilen"] + "' AND K.KISI_ID = Y.YETKILI_ID";
         
-        SqlQuery sqlquery2 = new SqlQuery();
-        DataTable datatable2 = sqlquery2.Query(query2);
-        TextAd .Text = datatable2.Rows[0][2].ToString();
-        TextSoyad.Text = datatable2.Rows[0][3].ToString();
-        TextDogum.Text = datatable2.Rows[0][4].ToString();
-        TextKan.Text = datatable2.Rows[0][5].ToString();
-        TextMail.Text = datatable2.Rows[0][6].ToString();
-        TextTel.Text = datatable2.Rows[0][7].ToString();
-        TextBrans.Text = datatable2.Rows[0][8].ToString();
-        TextID.ReadOnly = true;
-        TextTC.ReadOnly = true;
     }
 
     protected void ButtonSil_Click(object sender, EventArgs e)
