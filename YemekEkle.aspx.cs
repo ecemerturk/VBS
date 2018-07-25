895ec32c873335521e8f714ecaec4daa1b2ac9d0
Koralp <40428168+Koralp@users.noreply.github.com>
2018-07-25 17:55:47 +0300

    ü

--- YemekEkle.aspx.cs
+++ YemekEkle.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using sqlQuery;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,5 +12,13 @@ public partial class Default3 : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void Kaydet_Click(object sender, EventArgs e)
+    {
+        string query = "";
+        string n1 = System.Environment.NewLine;
+
+        SqlQuery sqlquery = new SqlQuery();
+        DataTable datatable = sqlquery.Query(query);
     }
 }
