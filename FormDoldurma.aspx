dbdf8fe3b51c415ef449ad4f48cf3081ac039f74
Ecem Ertürk <ecemerturk@users.noreply.github.com>
2018-07-26 14:31:31 +0300

    f

--- FormDoldurma.aspx
+++ FormDoldurma.aspx
@@ -3,19 +3,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
 <asp:Content ID="ContentPlaceHolder1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
-    <br />
-    <br />
-    <br />
-    <div class=" offset-md-3 align-content-center">
-        <div class="row">
-            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True"></asp:DropDownList>
-        </div>
-        <div class="row">
-            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True"></asp:DropDownList>
-        </div>
-        <div class="row">
-            <asp:DropDownList ID="DropDownList3" runat="server" AutoPostBack="True"></asp:DropDownList>
-        </div>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <script src="js/bootstrap.min.js"></script>
+    <div id="SoruCevapFormu" runat="server">
     </div>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="content1" runat="Server">
--- FormDoldurma.aspx.cs
+++ FormDoldurma.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
@@ -10,12 +11,13 @@ using System.Web.Security;
 using System.Configuration;
 using System.Collections;
 using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using System.ComponentModel;
 using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
 using sqlQuery;
+
+
 
 public partial class FormDoldurma : System.Web.UI.Page
 {
@@ -23,6 +25,83 @@ public partial class FormDoldurma : System.Web.UI.Page
     {
         string query = "";
         string nl = System.Environment.NewLine;
-        query += nl + "";
+        query += nl + "SELECT T.TSORU_ID, T.TSORU, T.TCEVAP_TIPI, C.SORU_ID, C.CEVAP";
+        query += nl + "FROM TAKIP_SORULARI T , CEVAP_SECENEK C";
+        query += nl + "WHERE T.TSORU_ID=C.SORU_ID";
+        SqlQuery sqlquery = new SqlQuery();
+        DataTable datatable = sqlquery.Query(query);
+        FormuDoldur(datatable);
+    }
+    private void FormuDoldur(DataTable datatable)
+    {
+        string AktifSoruId = "";
+
+
+        HtmlGenericControl FormTable = new HtmlGenericControl("div");
+        FormTable.Attributes.Add("class", "container offset-sm-3");
+        string count = "";
+
+        foreach (DataRow satir in datatable.Rows)
+        {
+
+            if (AktifSoruId != satir["SORU_ID"].ToString())
+            {
+                HtmlGenericControl SoruCevapGrubu = new HtmlGenericControl("div");
+                SoruCevapGrubu.Attributes.Add("class", "row");
+                FormTable.Controls.Add(SoruCevapGrubu);
+                HtmlGenericControl SoruCevapİçerikGrubu = new HtmlGenericControl("div");
+                SoruCevapİçerikGrubu.Attributes.Add("class", "col-sm-3");
+                SoruCevapGrubu.Controls.Add(SoruCevapİçerikGrubu);
+
+                HtmlGenericControl radioGrup = new HtmlGenericControl("div");
+                HtmlGenericControl radioLabel = new HtmlGenericControl("input");
+                radioLabel.Attributes.Add("type", "radio");
+                radioLabel.Attributes.Add("name", "radioGrup");
+                HtmlGenericControl checkboxLabel = new HtmlGenericControl("input");
+                checkboxLabel.Attributes.Add("type", "checkbox");
+                HtmlGenericControl acikucluCevap = new HtmlGenericControl("textarea");
+                //acikucluCevap.Attributes.Add("type", "text");
+                HtmlGenericControl bosluk = new HtmlGenericControl("br");
+
+                if (satir["TCEVAP_TIPI"].ToString() == "radio")
+                {
+                    if (count != satir["SORU_ID"].ToString())
+                    {
+                        SoruCevapİçerikGrubu.InnerText += satir["TSORU"].ToString();
+                    }
+                    SoruCevapİçerikGrubu.Controls.Add(bosluk);
+                    SoruCevapİçerikGrubu.Controls.Add(radioGrup);
+                    SoruCevapİçerikGrubu.Controls.Add(radioLabel);
+                    radioLabel.InnerText += satir["CEVAP"].ToString();
+                    count = satir["SORU_ID"].ToString();
+                    SoruCevapİçerikGrubu.Controls.Add(bosluk);
+                }
+                else if (satir["TCEVAP_TIPI"].ToString() == "check")
+                {
+                    if (count != satir["SORU_ID"].ToString())
+                    {
+                        SoruCevapİçerikGrubu.InnerText += satir["TSORU"].ToString();
+                    }
+                    SoruCevapİçerikGrubu.Controls.Add(bosluk);
+                    SoruCevapİçerikGrubu.Controls.Add(checkboxLabel);
+                    checkboxLabel.InnerText += satir["CEVAP"].ToString();
+                    count = satir["SORU_ID"].ToString();
+                    SoruCevapİçerikGrubu.Controls.Add(bosluk);
+                }
+                else if (satir["TCEVAP_TIPI"].ToString() == "textbox")
+                {
+                    if (count != satir["SORU_ID"].ToString())
+                    {
+                        SoruCevapİçerikGrubu.InnerText += satir["TSORU"].ToString();
+                    }
+                    SoruCevapİçerikGrubu.Controls.Add(bosluk);
+                    SoruCevapİçerikGrubu.Controls.Add(acikucluCevap);
+                    count = satir["SORU_ID"].ToString();
+                    SoruCevapİçerikGrubu.Controls.Add(bosluk);
+                }
+            }
+            
+        }
+        SoruCevapFormu.Controls.Add(FormTable);
     }
 }
